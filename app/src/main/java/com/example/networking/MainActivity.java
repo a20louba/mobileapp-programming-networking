4f0e37960f49815edd08cca88f74cfea444655fd
a20louba <a20louba@student.his.se>
2022-05-04 15:31:07 +0200

    förberett för internet, la till rätt url. la till JsonTask, för att hämta från nätet.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,30 +2,23 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
-
-    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            new JsonTask(this).execute(JSON_URL);
 
-        recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setAdapter(new MyAdapter());
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        new JsonFile(this, this).execute(JSON_FILE);
+       // new JsonFile(this, this).execute(JSON_FILE);
     }
 
     @Override
